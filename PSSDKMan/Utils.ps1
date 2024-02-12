1984535b90ce1649165206e73441ffaa0335e9fb
Ronny Polley <ronnypolley@googlemail.com>
2024-02-12 18:16:26 +0100

    replace alias with commandlet

--- PSSDKMan/Utils.ps1
+++ PSSDKMan/Utils.ps1
@@ -255,7 +255,7 @@ function Test-Is-Candidate-Version-Locally-Available($Candidate, $Version) {
 }
 
 function Get-Installed-Candidate-Version-List($Candidate) {
-    return Get-ChildItem "$Global:PSDK_DIR\$Candidate" | ?{ $_.PSIsContainer -and $_.Name -ne 'current' } | Foreach { $_.Name }
+    return Get-ChildItem "$Global:PSDK_DIR\$Candidate" | ?{ $_.PSIsContainer -and $_.Name -ne 'current' } | ForEach-Object { $_.Name }
 }
 
 function Get-Online-Candidate-Version-List($Candidate) {
